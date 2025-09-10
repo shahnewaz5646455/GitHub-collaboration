0bb58596725008121e6842d489bb3447e6713699
shahnewaz5646455 <shahnewaz794@gmail.com>
2025-09-10 17:39:39 +0600

    i am adding a massage

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="font-bold">i k am here</h1>
-    </div>
+   <div>
+    <h1>
+      this is nothing
+    </h1>
+   </div>
   );
 }
